031a3824ba0f1ef2b1f0b80e68cc86967a4a9c32
Ubuntu <ubuntu@ip-172-31-43-254.us-west-2.compute.internal>
2020-11-12 05:41:26 +0000

    modify EC2 to t2.micro

--- create-k8s-cluster.sh
+++ create-k8s-cluster.sh
@@ -4,8 +4,8 @@ eksctl create cluster \
 --name minipaintCluster \
 --version 1.17 \
 --region us-west-2 \
---node-type t2.small \
+--node-type t2.micro \
 --nodes 2 \
 --nodes-min 1 \
---nodes-max 4 \
+--nodes-max 3 \
 --managed
